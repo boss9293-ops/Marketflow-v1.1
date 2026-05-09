bdd98b169bc31351b3134ade112eb3dd833ab9ea
MarketFlow Dev <dev@marketflow.local>
2026-05-09 09:05:27 -0500

    Add semiconductor-lens app route page

---
+++ marketflow/frontend/src/app/semiconductor-lens/page.tsx
@@ -0,0 +1,5 @@
+import { SemiconductorIntelligenceShell } from '@/components/semiconductor/SemiconductorIntelligenceShell'
+
+export default function SemiconductorLensPage() {
+  return <SemiconductorIntelligenceShell />
+}
